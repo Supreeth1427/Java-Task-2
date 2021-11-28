b6ae55627e277e35604553b74aabf3af1863ebec
Supreeth1427 <88834108+Supreeth1427@users.noreply.github.com>
2021-11-28 23:35:43 +0530

    Update ArrayList.java

--- ArrayList.java
+++ ArrayList.java
@@ -4,7 +4,7 @@ class ArrayList
   public static void main(String args[])
   {
     Scanner sc = new Scanner(System.in);
-    System.out.print("Enter the number of elements:")
+    System.out.print("Enter the number of elements:");
     int n = sc.nextInt();
     ArrayList<Integer> ar = new ArrayList<>();
     for(int i=0;i<n;i++)
