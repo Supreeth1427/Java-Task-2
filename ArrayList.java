562f5421b555a06d4e9c78151f8bcb893aec8735
Supreeth1427 <88834108+Supreeth1427@users.noreply.github.com>
2021-11-29 20:26:50 +0530

    Update ArrayList.java

--- ArrayList.java
+++ ArrayList.java
@@ -6,14 +6,14 @@ class ArrayList
     Scanner sc = new Scanner(System.in);
     System.out.print("Enter the number of elements:");
     int n = sc.nextInt();
-    ArrayList<Integer> ar = new ArrayList<>();
+    List<Integer> li = new ArrayList<>();
     for(int i=0;i<n;i++)
     {
-      ar.add(sc.nextInt());
+      li.add(sc.nextInt());
     }
     System.out.print("Enter the element to be removed");
     int del = sc.nextInt();
-    boolean r = ar.remove(del);
-    System.out.print(r);
+    li.remove(new Integer(del));
+    System.out.print(li);
   }
 }
